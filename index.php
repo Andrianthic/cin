e42e9f4fc6bbb5d512d41c6aa82f0bc0971eafa6
Andrianthic <andrianthic@gmail.com>
2017-08-16 15:52:51 +0200

    formulaire-simple

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>CIN - Madagascar</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
+<div class="form">
+	<h1>Ajouter vos informations</h1>
+	<form method="POST" action="">
+		<table>
+			<tr><td class="td1">Nom</td><td><input type="text" name="name"></td></tr>
+			<tr><td class="td1">Prénom</td><td><input type="text" name="name"></td></tr>
+			<tr><td class="td1">Date de naissance</td><td><input type="text" name="name"></td></tr>
+			<tr><td class="td1">Lieu de naissance</td><td><input type="text" name="name"></td></tr>
+			<tr><td class="td1">Num CIN</td><td><input type="text" name="name"></td></tr>
+			<tr class="tr-last"><td class="td1"></td><td><button>Enregistrer</button><button>Annuler</button></td></tr>
+		</table>
+	</form>
+</div>
 </body>
 </html>
