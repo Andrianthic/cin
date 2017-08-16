103185a9bc4ecdc794c5940d1a74d44cdb9bacd1
Rocknatt <fahernatt@gmail.com>
2017-08-16 16:25:45 +0300

    Corriger bug index

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php 
+	$bdd = new PDO('mysql:host=localhost;dbname=cin;charset=utf8', 'root', '');
+
+	if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['date_naissance']) && isset($_POST['lieu_naissance']) && isset($_POST['num_cin'])) {
+
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $date_naissance = $_POST['date_naissance'];
+            $lieu_naissance = $_POST['lieu_naissance'];
+            $num_cin = $_POST['num_cin'];
+
+            $req_ajout= $bdd->prepare('INSERT INTO carte(nom, prenom, date_naissance, lieu_naissance, num_cin, date_ajout)VALUES(:nom, :prenom, :date_naissance, :lieu_naissance, :num_cin, NOW())');
+            $req_ajout->execute(array(
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'date_naissance' => $date_naissance,
+            'lieu_naissance' => $lieu_naissance,
+            'num_cin' => $num_cin,
+            ));
+
+            header('Location: index.php');
+        }
+
+    
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
