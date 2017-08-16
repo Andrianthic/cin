ee539c2a22692f8a6f2c74c31f38527ee2a90979
Andrianthic <andrianthic@gmail.com>
2017-08-16 16:32:10 +0200

    new style

--- index.php
+++ index.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
+
 <html>
 <head>
 	<title>CIN - Madagascar</title>
+	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<div class="form">
-	<h1>Ajouter vos informations</h1>
-	<form method="POST" action="">
-		<table>
-			<tr><td class="td1">Nom</td><td><input type="text" name="name"></td></tr>
-			<tr><td class="td1">Prénom</td><td><input type="text" name="name"></td></tr>
-			<tr><td class="td1">Date de naissance</td><td><input type="text" name="name"></td></tr>
-			<tr><td class="td1">Lieu de naissance</td><td><input type="text" name="name"></td></tr>
-			<tr><td class="td1">Num CIN</td><td><input type="text" name="name"></td></tr>
-			<tr class="tr-last"><td class="td1"></td><td><button>Enregistrer</button><button>Annuler</button></td></tr>
-		</table>
-	</form>
+<div class="formulaire">
+	<div class="titre">
+		<h1>Ajouter vos informations</h1>
+	</div>
+	<div class="form">
+		<form method="POST" action="">
+			<table>
+				<tr><td class="td1">Nom</td><td><input type="text" name="name"></td></tr>
+				<tr><td class="td1">Prénom</td><td><input type="text" name="name"></td></tr>
+				<tr><td class="td1">Date de naissance</td><td><input type="text" name="name"></td></tr>
+				<tr><td class="td1">Lieu de naissance</td><td><input type="text" name="name"></td></tr>
+				<tr><td class="td1">Num CIN</td><td><input type="text" name="name"></td></tr>
+				<tr class="tr-last"><td class="td1"></td><td><button>Enregistrer</button><button>Annuler</button></td></tr>
+			</table>
+		</form>
+	</div>
 </div>
 </body>
 </html>
